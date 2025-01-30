e1ec899ffd26d53f4ff3c8a77c35136990725c32
StudentSubrata <student_subrata@outlook.com>
2025-01-31 02:47:43 +0600

    App version 1.0.0

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const tools = {
 
 let painting = false;
 let brushSize = 1;
-let brushColor = "#081abd";
+let opacity = 1.0;
+let brushColor = `rgba(0,0,0,${opacity})`;
 let currentTool = "brush";
 let startX, startY;
 let snapshot; // To store canvas state before drawing a rectangle
@@ -58,13 +59,11 @@ function draw(e) {
 
     switch (currentTool) {
         case "horizontalLine":
-            ctx.lineTo(e.offsetX, startY);
-            ctx.stroke();
+            ctx.strokeRect(startX, startY, e.offsetX - startX, 1);
             break;
 
         case "verticalLine":
-            ctx.lineTo(startX, e.offsetY);
-            ctx.stroke();
+            ctx.strokeRect(startX, startY, 1, e.offsetY - startY);
             break;
 
         case "rectangle":
@@ -125,11 +124,13 @@ tools.circle.addEventListener("click", () => setActiveTool("circle"));
 tools.filledCircle.addEventListener("click", () => setActiveTool("filledCircle"));
 
 colorPicker.addEventListener("input", (e) => {
-    brushColor = e.target.value;
+    brushColor = createRGBA(e.target.value, opacity);
 });
 
 brushSizeInput.addEventListener("input", (e) => {
     brushSize = e.target.value;
+    document.getElementById("rangeValue").textContent =
+        parseInt(brushSize) >= 10 ? brushSize : `0${brushSize}`;
 });
 
 // Clear Canvas
@@ -144,3 +145,35 @@ document.getElementById("save").addEventListener("click", () => {
     link.download = "paint.png";
     link.click();
 });
+
+document.getElementById("opacity").addEventListener("input", (e) => {
+    opacity = e.target.value;
+    document.getElementById("opacityValue").textContent = opacity;
+
+    const { r, g, b } = extractRGB(brushColor);
+    brushColor = `rgba(${r}, ${g}, ${b}, ${opacity})`;
+});
+
+function createRGBA(hex, opacity) {
+    // Remove the '#' if it's present
+    hex = hex.replace("#", "");
+    // Convert the hex color to RGB
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
+function extractRGB(rgbaString) {
+    // Regular expression to match rgba values
+    const rgbaRegex = /rgba\((\d+),\s*(\d+),\s*(\d+),\s*([\d.]+)\)/;
+
+    const match = rgbaString.match(rgbaRegex);
+
+    const r = parseInt(match[1], 10);
+    const g = parseInt(match[2], 10);
+    const b = parseInt(match[3], 10);
+
+    return { r, g, b };
+}
